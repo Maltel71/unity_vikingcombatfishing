69878eac587a0ca03b0f7b0cf78f9a25cf739c77
AnkanVL <v.langelotz@gmail.com>
2026-01-19 14:36:48 +0100

    Created Player script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] public int playerHealth = 100;
+    [SerializeField] public float playerSpeed = 5f;
+    [SerializeField] public string playerName = "Ragnar";
+    [SerializeField] public float AttackSpeed = 1.5f;
+    [SerializeField] 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
